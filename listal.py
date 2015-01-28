99d52b957a79e13cc7af18eb343fa44b77136db6
Mariano Simone <marianosimone@gmail.com>
2015-01-28 15:42:19 -0500

    add url + cleanup

--- listal.py
+++ listal.py
@@ -1,7 +1,6 @@
 from bs4 import BeautifulSoup
 import urllib2
 import re
-from datetime import datetime
 
 
 _image_finder = re.compile(r"img src='(.*)'")
@@ -31,15 +30,20 @@ _status = {
 }
 
 
-def _get_items(url):
-    data = BeautifulSoup(urllib2.urlopen(url).read())
-    items = data.rss.find_all('item')
-    return [{
+def guess_attributes(item):
+    return {
         'title': item.find('title').text,
         'image': _image_finder.search(item.find('description').text).groups()[0],
         'status': _statuses[item.find('listal:used').text],
-        'since': item.find('pubdate').text
-    } for item in items]
+        'since': item.find('pubdate').text,
+        'url': item.find('link').text,
+    }
+
+
+def _get_items(url):
+    data = BeautifulSoup(urllib2.urlopen(url).read())
+    items = data.rss.find_all('item')
+    return [guess_attributes(item) for item in items]
 
 
 def _get_data(user, collection, ownership, status):

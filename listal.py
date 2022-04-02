19e47b35c52dee75aa6d0d441cea5f9c4aad99f4
Mariano Simone <marianosimone@gmail.com>
2022-04-02 09:07:01 -0600

    Lint with black

--- listal.py
+++ listal.py
@@ -6,71 +6,77 @@ import re
 _image_finder = re.compile(r"img src='(.*)'")
 
 _statuses = {
-    'Yes': 'used',
-    'Using': 'using',
-    'No': 'unused',
-    'used': 'Yes',
-    'using': 'Using',
-    'unused': 'No',
-    '': 'unused'
+    "Yes": "used",
+    "Using": "using",
+    "No": "unused",
+    "used": "Yes",
+    "using": "Using",
+    "unused": "No",
+    "": "unused",
 }
 
 _status = {
-    'books': {
-        'wanted': ('wanted',),
-        'owned': ('owned',),
-        'used': ('read',),
-        'all': ('owned', 'wanted', 'read')
+    "books": {
+        "wanted": ("wanted",),
+        "owned": ("owned",),
+        "used": ("read",),
+        "all": ("owned", "wanted", "read"),
     },
-    'movies': {
-        'wanted': ('wanted',),
-        'used': ('watched',),
-        'all': ('wanted', 'watched')
-    }
+    "movies": {
+        "wanted": ("wanted",),
+        "used": ("watched",),
+        "all": ("wanted", "watched"),
+    },
 }
 
 
 def guess_attributes(item):
     return {
-        'title': item.find('title').text,
-        'image': _image_finder.search(item.find('description').text).groups()[0],
-        'status': _statuses[item.find('listal:used').text],
-        'since': item.find('pubdate').text,
-        'url': item.find('link').text,
+        "title": item.find("title").text,
+        "image": _image_finder.search(item.find("description").text).groups()[0],
+        "status": _statuses[item.find("listal:used").text],
+        "since": item.find("pubdate").text,
+        "url": item.find("link").text,
     }
 
 
 def _get_items(url):
     data = BeautifulSoup(urlopen(url).read(), "html.parser")
-    items = data.rss.find_all('item')
+    items = data.rss.find_all("item")
     return [guess_attributes(item) for item in items]
 
 
 def _get_data(user, collection, ownership, status):
-    base_url = 'https://%s.listal.com/rss/%s/%s/1?sortby=dateadded-desc'  # TODO: Handle paging
+    base_url = (
+        "https://%s.listal.com/rss/%s/%s/1?sortby=dateadded-desc"  # TODO: Handle paging
+    )
     items = []
-    used_query = ('&used=%s' % _statuses[status] ) if status != 'all' else ''
+    used_query = ("&used=%s" % _statuses[status]) if status != "all" else ""
     for s in _status[collection][ownership]:
         items.extend(_get_items((base_url % (user, collection, s)) + used_query))
     return items
 
+
 def reading(user):
-    return _get_data(user, 'books', 'all', 'using')
+    return _get_data(user, "books", "all", "using")
+
 
 def read(user):
-    return _get_data(user, 'books', 'used', 'all')
+    return _get_data(user, "books", "used", "all")
+
 
 def _extract_list_element_data(row):
-    image, content = row.find_all('div', recursive=False)
-    first, second = content.find_all('div', recursive=False)
+    image, content = row.find_all("div", recursive=False)
+    first, second = content.find_all("div", recursive=False)
     return {
-        'image': image.find('img')['src'],
-        'title': first.find('a').text,
-        'url': first.find('a')['href'],
-        'comment': second.find('div').decode_contents(formatter='html')
+        "image": image.find("img")["src"],
+        "title": first.find("a").text,
+        "url": first.find("a")["href"],
+        "comment": second.find("div").decode_contents(formatter="html"),
     }
 
+
 def list_details(name):
-    data = BeautifulSoup(urlopen('https://www.listal.com/list/%s' % name).read())
-    rows = data.body.find_all(attrs={'class': 'notesrow'})
+    data = BeautifulSoup(urlopen("https://www.listal.com/list/%s" % name).read())
+    rows = data.body.find_all(attrs={"class": "notesrow"})
     return [_extract_list_element_data(row) for row in rows]
--- main.py
+++ main.py
@@ -7,55 +7,61 @@ import listal
 
 
 class EnableCors(object):
-    name = 'enable_cors'
+    name = "enable_cors"
     api = 2
 
     def apply(self, fn, context):
         def _enable_cors(*args, **kwargs):
             # set CORS headers
-            response.headers['Access-Control-Allow-Origin'] = '*'
-            response.headers['Access-Control-Allow-Methods'] = 'GET'
-            response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+            response.headers["Access-Control-Allow-Origin"] = "*"
+            response.headers["Access-Control-Allow-Methods"] = "GET"
+            response.headers[
+                "Access-Control-Allow-Headers"
+            ] = "Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token"
 
-            if request.method != 'OPTIONS':
+            if request.method != "OPTIONS":
                 # actual request; reply with the actual response
                 return fn(*args, **kwargs)
 
         return _enable_cors
 
+
 app = bottle_app()
 app.install(EnableCors())
 
-@app.route('/')
+
+@app.route("/")
 def handle_root_url():
-    return static_file('index.html', root='views')
+    return static_file("index.html", root="views")
 
-ACTIONS = {
-    'reading': listal.reading,
-    'read': listal.read
-}
 
-@app.route('/user/<user>/<action:re:[\w]+><is_json:re:(\.json)?>')
+ACTIONS = {"reading": listal.reading, "read": listal.read}
+
+
+@app.route("/user/<user>/<action:re:[\w]+><is_json:re:(\.json)?>")
 def serve(user, action, is_json):
     if action in ACTIONS:
         items = ACTIONS[action](user)
         if is_json:
-            return {'items': items}
-        return template('items.tpl', items=items)
+            return {"items": items}
+        return template("items.tpl", items=items)
     abort(404)
 
-@app.route('/list/<name:re:[\w-]+><is_json:re:(\.json)?>')
+
+@app.route("/list/<name:re:[\w-]+><is_json:re:(\.json)?>")
 def list_details(name, is_json):
     items = listal.list_details(name)
     if is_json:
-        return {'items': items}
-    return template('items.tpl', items=items)
+        return {"items": items}
+    return template("items.tpl", items=items)
+
 
 @app.error(404)
 def error404(error):
-    return template('error', error_msg='404 error. Nothing to see here')
+    return template("error", error_msg="404 error. Nothing to see here")
 
-if os.environ.get('APP_LOCATION') == 'heroku':
+
+if os.environ.get("APP_LOCATION") == "heroku":
     run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 else:
-    run(app, host='localhost', port=8080, debug=True, reloader=True)
+    run(app, host="localhost", port=8080, debug=True, reloader=True)
